5e922d3b636643140a55c8b455725b2fd5d6c7f8
SteveJDeveloper <103290510+SteveJDeveloper@users.noreply.github.com>
2022-07-06 13:55:43 -0500

    Finalizado

--- client/src/pages/Funcionalidad.jsx
+++ client/src/pages/Funcionalidad.jsx
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import ListPases from './ListPases';
+import ListUsuarios from './ListUsuarios';
+import FormularioUser from './FormularioUser';
+import Reporte from './Reporte';
 
 export const Funcionalidad = () => {
-    const getData = async () => {
-        axios.get('/api/pases').then((response) => {
-            console.log(response)
-        })
-    }
-    useEffect(() => {
-        getData();
-    }, []);
     return (
-        <div className='container'>
-            <div className='abs-center'><h2>Bienvenido a SIP - Sistema Informatico de Pases</h2></div>
+        <div className='container'><br />
+            <div className='abs-center'><h2>Bienvenido a SIP - Sistema Informatico de Pases</h2></div><br />
             <div className='row'>
                 <div className='col-md-6'>
-                    <h3>Usuarios</h3>
+                    <h3>Usuarios</h3><br />
+                    <FormularioUser />
+                    <ListUsuarios />
                 </div>
                 <div className='col-md-6'>
-                    <h3>Pases</h3>
+                    <h3>Pases</h3><br />
+                    <ListPases />
                 </div>
             </div>
             <div className='row'>
                 <div className='col-md-12'>
                     <h3>Reporte</h3>
+                    <Reporte />
                 </div>
             </div>
         </div>
